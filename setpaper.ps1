dd3d4c086c24ff52476f29b7591db82b4fdeabe9
John Norton <here@spindlecrank.com>
2024-10-01 14:38:38 -0500

    Update setpaper.ps1
    Add check for landscape image

--- setpaper.ps1
+++ setpaper.ps1
@@ -1,22 +1,37 @@
 # Define the path to the images folder
 $imagePath = "C:\path\to\your\images"
 
-# Function to select a random image from the predefined folder, filtering out portrait-oriented ones
-function Get-RandomLandscapeImage {
+# Function to check if an image is in landscape orientation
+function Is-Landscape($imagePath) {
     try {
-        # Get all .png and .jpg files in the directory that are landscape-oriented (width > height)
-        $landscapeImages = Get-ChildItem -Path $imagePath -Filter "*.png", "*.jpg" | Where-Object { ([System.Drawing.Image]::FromFile($_.FullName)).GetWidth() -gt ([System.Drawing.Image]::FromFile($_.FullName)).GetHeight()
+        $bitmap = [System.Drawing.Bitmap]::FromFile($imagePath)
+        return $bitmap.Width -gt $bitmap.Height
+    } catch {
+        Write-Error "Failed to load image: $_"
+        return $false
+    }
+}
 
-        if ($landscapeImages.Count -eq 0) {
-            Write-Error "No landscape images found in the folder."
+# Function to select a random image from the predefined folder
+function Get-RandomImage {
+    try {
+        # Get all .png and .jpg files in the directory
+        $images = Get-ChildItem -Path $imagePath -Filter "*.png", "*.jpg" | Sort-Object LastWriteTime -Descending
+        
+        if ($images.Count -eq 0) {
+            Write-Error "No images found in the folder."
             return
         }
-
-        # Select a random image from the list of landscape-oriented ones
-        $randomImage = Get-Random -Minimum 0 -Maximum $landscapeImages.Count
-        $selectedImage = $landscapeImages[$randomImage]
-
-        return $selectedImage.FullName
+        
+        while ($true) {
+            # Select a random image from the list
+            $randomImage = Get-Random -Minimum 0 -Maximum $images.Count
+            $selectedImage = $images[$randomImage]
+            
+            if (Is-Landscape $selectedImage.FullName) {
+                return $selectedImage.FullName
+            }
+        }
     } catch {
         Write-Error "An error occurred while selecting an image: $_"
         return
@@ -30,7 +45,7 @@ function Set-DesktopWallpaper($imagePath) {
             # Set the image as the desktop wallpaper using PowerShell's built-in function
             [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
             $wallpaperStyle = 22 # Windows style: stretched
-            [Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "WallPaperStyle", $wallpaperStyle)
+            [Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "WallpaperStyle", $wallpaperStyle)
             [Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Control Panel\Colors", "Background", 0x000000)
         } else {
             Write-Error "The specified image path does not exist."
@@ -42,7 +57,7 @@ function Set-DesktopWallpaper($imagePath) {
 
 # Main script execution
 try {
-    $image = Get-RandomLandscapeImage
+    $image = Get-RandomImage
     if ($image) {
         Set-DesktopWallpaper -imagePath $image
         Write-Output "Successfully set the wallpaper to $image."
